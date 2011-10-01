088cb3c19757da4d23391cf8ac50fe0d532bb3a4
Matthew Chambers <Matthew@Chambers.net.nz>
2011-10-02 00:52:36 +1300

    Tests for the dynamic implementation

---
+++ Construct.NET.Tests/DynamicTests.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Construct.NET.Tests
+{
+    [TestFixture]
+    class DynamicTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            dynamic ctr = new DynamicConstruct();
+            ctr.Value = CtrType.Int32;
+            byte[] data = { 0xff, 0x00, 0x00, 0x00 };
+            using (var memStream = new MemoryStream(data))
+            {
+                memStream.Seek(0, SeekOrigin.Begin);
+                ctr.Parse(memStream);
+                const int expected = 255;
+
+                Assert.AreEqual(expected, ctr.Value);
+            }
+            
+        }
+    }
+}
